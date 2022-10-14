3611dd15e61f5707f57335ff97f4aa28509c4b49
Sofya Dobychina <96266344+sf-d@users.noreply.github.com>
2022-10-14 04:58:57 +0300

    blааввв

---
+++ topics/internal.py
@@ -0,0 +1,34 @@
+class Внутреннее:
+
+    def __init__(self):
+        super().__init__()
+
+
+class Форма(Внутреннее):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Контекст(Внутреннее):
+
+    def __init__(self):
+        super().__init__()
+
+
+class История(Внутреннее):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Система(Внутреннее):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Материальность(Внутреннее):
+
+    def __init__(self):
+        super().__init__()
